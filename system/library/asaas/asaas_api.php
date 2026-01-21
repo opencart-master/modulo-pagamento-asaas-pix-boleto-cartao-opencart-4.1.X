5bd70d10781cfd49e4d7d7d2a8d1fa0b9a6a12a3
deniscunha <ded_2004@hotmail.com>
2026-01-21 15:10:26 -0300

    Update API

--- system/library/asaas/asaas_api.php
+++ system/library/asaas/asaas_api.php
@@ -4,11 +4,13 @@ class AsaasApi {
     private $api_key;
     private $base_url;
     private $token;
+    private $origin;
 
     public function __construct($api_key, $sandbox = true) {
         $this->api_key = $api_key;
         $this->base_url = $sandbox ? 'https://sandbox.asaas.com/api/v3/' : 'https://www.asaas.com/api/v3/';
         $this->token = $sandbox ?  base64_decode('JGFzYWFzX2hvbW9sb2dfb3JpZ2luX2NoYW5uZWxfa2V5X05UaG1OemxpWVdSaE1tVTFPRFZoWm1KbE1qazVNMlJsWXpnd05qTmxaR1U2T2pnM09HUTBaV1V4TFRBek1XRXRORGxoWkMwNU5qZzNMVE5tT1dWaE5HSTNZek5tTnpvNmIyTnJhR1U1T0RVeE0yVTBMVGc0WlRRdE5HWmtaaTA1TldKbExXRmxaRGMwT1RZMFpEVmxPUT09') : base64_decode('JGFzYWFzX3Byb2Rfb3JpZ2luX2NoYW5uZWxfa2V5X05UaG1OemxpWVdSaE1tVTFPRFZoWm1KbE1qazVNMlJsWXpnd05qTmxaR1U2T2pjd01XUXdOR1ExTFRFd1l6TXRORGcwTmkwNFpHVmxMVFEyTm1GalptSXhNekZpTVRvNmIyTnJhRE5tWkRBeVltVmhMV1ZqWXpjdE5HUTROQzFoTURFMkxXRTBOemMxTVRaak1ESTNaZz09');
+        $this->origin = base64_decode('T1BFTkNBUlRfTUFTVEVS');
     }
 
     private function request($method, $endpoint, $data = []) {
@@ -16,6 +18,7 @@ class AsaasApi {
         curl_setopt($soap_do, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($soap_do, CURLOPT_HTTPHEADER, [
             'Content-Type: application/json',
+            'Origin: ' . $this->origin,
             'Origin-Channel-Access-Token: ' . $this->token,
             'User-Agent: ' . base64_decode('TWFzdGVyLzEuMC4wLjAgKFBsYXRhZm9ybWEgb3BlbmNhcnQuY29tIC0gREVWIE9wZW5jYXIgTWFzdGVyKQ=='),
             'access_token: ' . $this->api_key
