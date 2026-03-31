d4953fb8ba7c0300688959e9e15dd4baaa83ef55
deniscunha <ded_2004@hotmail.com>
2026-03-31 08:53:46 -0300

    adjust event check version

--- admin/controller/event/version.php
+++ admin/controller/event/version.php
@@ -4,7 +4,7 @@ namespace Opencart\Admin\Controller\Extension\Asaas\Event;
 class Version extends \Opencart\System\Engine\Controller {
     
 	public function index(string &$route, array &$args, mixed &$output): void {
-	    $version = '1.0.0.0';
+	    $version = '1.0.0.1';
 	    $url = base64_decode('aHR0cHM6Ly9vcGVuY2FydG1hc3Rlci5jb20uYnIvbW9kdWxlL3ZlcnNpb24v');
         $soap_do = curl_init();
         curl_setopt($soap_do, CURLOPT_URL, $url);
@@ -19,7 +19,7 @@ class Version extends \Opencart\System\Engine\Controller {
         curl_close($soap_do);
         $resposta = json_decode($response, true);
         if ($resposta['asaas'] > $version) {
-            echo '<div class="alert alert-info" role="alert">Atenção: tem uma nova versão do módulo <b>Asaas</b>, disponível para <a href="https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=48210&filter_search=asaas" target="_blank">download</a>!</div>';
+            echo '<div class="alert alert-info" role="alert">Atenção: tem uma nova versão do módulo <b>Asaas</b>, disponível para <a href="https://www.opencart.com/index.php?route=marketplace/extension/info&extension_id=48210" target="_blank">download</a>!</div>';
     
         } else {
            echo "";
